3c700f308b6a847e946ba8fd1ed9e0975cf32dcf
无始 <lwang95@leqee.com>
2022-04-14 09:35:14 +0800

    fix: grouped-bar

--- src/components/em/grouped-bar.vue
+++ src/components/em/grouped-bar.vue
@@ -30,13 +30,63 @@ export default {
   &::-webkit-scrollbar {
     height: 8px;
   }
+
   &::-webkit-scrollbar-track {
-    border-radius: 4px;
     background: #fff;
+    border-radius: 4px;
   }
+
   &::-webkit-scrollbar-thumb {
     background: #c7c6dc;
     border-radius: 4px;
+  }
+}
+
+.d3-gmv-tooltip {
+  z-index: 0;
+  display: flex;
+  width: 156px;
+  height: 80px;
+  flex-direction: column;
+  justify-content: center;
+  padding: 6px 10px;
+  background: rgba(63, 61, 88, 0.85);
+  border-radius: 4px;
+  font-size: 12px;
+  pointer-events: none;
+
+  &-target {
+    width: 6px;
+    height: 6px;
+    background: #ff8b7b;
+    border-radius: 2px;
+  }
+
+  &-real {
+    width: 6px;
+    height: 6px;
+    background: #877bf4;
+    border-radius: 2px;
+  }
+
+  &-line {
+    width: 12px;
+    height: 2px;
+    background: #bab4ef;
+    border-radius: 2px;
+  }
+
+  &-title {
+    width: 53px;
+  }
+
+  & > p {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    margin: 0;
+    color: #fff;
+    gap: 5px;
   }
 }
 </style>
